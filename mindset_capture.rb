0b9ca78e08166936e903be5e5c9aa3deaa43c3f8
mkfs <semper.non.sequitur@gmail.com>
2013-05-02 01:01:01 -0400

    Extracted ruby module from mindset_capture

--- mindset_capture.rb
+++ mindset_capture.rb
@@ -8,127 +8,193 @@
 require 'ostruct'
 require 'optparse'
 
-require 'rubygems'                    # gem install serialport
-require 'serialport'
-require 'json/ext'
-
-# ----------------------------------------------------------------------
-DEFAULT_SERIAL_PORT = "/dev/rfcomm0"
-MINDSET_BAUD = 57600
-
-BT_SYNC = 0xAA
-EXCODE = 0x55
-
-CODE_SIGNAL_QUALITY = 0x02 # POOR_SIGNAL quality 0-255
-CODE_ATTENTION = 0x04      # ATTENTION eSense 0-100
-CODE_MEDITATION = 0x05     # MEDITATION eSense 0-100
-CODE_BLINK = 0x16          # BLINK strength 0-255
-CODE_WAVE = 0x80           # RAW wave value: 2-byte big endian twos-complement
-CODE_ASIC_EEG = 0x83       # ASIC EEG POWER 8 3-byte big-endian integers
-
-class ConnectionTimeoutError < RuntimeError; end
+require 'mindset'
 
 # ----------------------------------------------------------------------
 # Treat 3-element array as a 3-byte integer in big-endian order
 # Note: This zero-pads the integer to 4-bytes before calling unpack, which
 #       may not be correct if the values can be signed.
-def unpack_3byte_bigendian(arr)
-  arr.unshift(0).pack('cccc').unpack('L>').first
-end
-
-def unpack_asic_eeg(arr)
-  { :delta => unpack_3byte_bigendian(arr[0,3]),
-    :theta => unpack_3byte_bigendian(arr[3,3]),
-    :lo_alpha => unpack_3byte_bigendian(arr[6,3]),
-    :hi_alpha => unpack_3byte_bigendian(arr[9,3]),
-    :lo_beta => unpack_3byte_bigendian(arr[12,3]),
-    :hi_beta => unpack_3byte_bigendian(arr[15,3]),
-    :lo_gamma => unpack_3byte_bigendian(arr[18,3]),
-    :mid_gamma => unpack_3byte_bigendian(arr[21,3])
-    }
-end
-
-def decode_data_row(excode, code, value, verbose=nil)
-  # note: currently, excode is ignored
-  case code
-  when CODE_SIGNAL_QUALITY
-    { :signal_quality => value.first }
-  when CODE_ATTENTION
-    { :attention => value.first }
-  when CODE_MEDITATION
-    { :meditation => value.first }
-  when CODE_BLINK
-    { :blink => value.first }
-  when CODE_WAVE
-    { :wave => value[0,2].join('').unpack("s>").first }
-  when CODE_ASIC_EEG
-    unpack_asic_eeg(value[0,24])
-  else
-    $stderr.puts "Unrecognized code: %02X" % code if verbose
+=begin
+  def unpack_3byte_bigendian(arr)
+    arr.unshift(0).pack('cccc').unpack('L>').first
   end
-end
 
-def parse_packet(bytes, verbose=false)
-  packets = []
-  while not bytes.empty?
-    excode = 0
-    while bytes[0] == EXCODE
-      excode += 1
-      bytes.shift
+  def unpack_asic_eeg(arr)
+    { :delta => unpack_3byte_bigendian(arr[0,3]),
+      :theta => unpack_3byte_bigendian(arr[3,3]),
+      :lo_alpha => unpack_3byte_bigendian(arr[6,3]),
+      :hi_alpha => unpack_3byte_bigendian(arr[9,3]),
+      :lo_beta => unpack_3byte_bigendian(arr[12,3]),
+      :hi_beta => unpack_3byte_bigendian(arr[15,3]),
+      :lo_gamma => unpack_3byte_bigendian(arr[18,3]),
+      :mid_gamma => unpack_3byte_bigendian(arr[21,3])
+      }
+  end
+
+  def decode_data_row(excode, code, value, verbose=nil)
+    # note: currently, excode is ignored
+    case code
+    when CODE_SIGNAL_QUALITY
+      { :signal_quality => value.first }
+    when CODE_ATTENTION
+      { :attention => value.first }
+    when CODE_MEDITATION
+      { :meditation => value.first }
+    when CODE_BLINK
+      { :blink => value.first }
+    when CODE_WAVE
+      { :wave => value[0,2].join('').unpack("s>").first }
+    when CODE_ASIC_EEG
+      unpack_asic_eeg(value[0,24])
+    else
+      $stderr.puts "Unrecognized code: %02X" % code if verbose
+    end
+  end
+
+  def parse_packet(bytes, verbose=false)
+    packets = []
+    while not bytes.empty?
+      excode = 0
+      while bytes[0] == EXCODE
+        excode += 1
+        bytes.shift
+      end
+
+      code = bytes.shift
+      vlen = (code >= 0x80) ? bytes.shift : 1
+      value = bytes.slice! 0, vlen
+      packets << decode_data_row(excode, code, value, verbose)
     end
 
-    code = bytes.shift
-    vlen = (code >= 0x80) ? bytes.shift : 1
-    value = bytes.slice! 0, vlen
-    packets << decode_data_row(excode, code, value, verbose)
+    packets
   end
 
-  packets
-end
 # ----------------------------------------------------------------------
-def packets_to_json( h, options )
-  # TODO: something more sophisticated
-  h.to_json
+  def packets_to_json( h, options )
+    # TODO: something more sophisticated
+    h.to_json
+  end
+
+  def new_packet_store
+    { 
+      :start_ts => Time.now,
+      :end_ts => nil,
+      :delta => [],
+      :theta => [],
+      :lo_alpha => [],
+      :hi_alpha => [],
+      :lo_beta => [],
+      :hi_beta => [],
+      :lo_gamma => [],
+      :mid_gamma => [],
+      :signal_quality => [],
+      :attention => [],
+      :meditation => [],
+      :blink => [],
+      :wave => []
+    }
+  end
+
+  def is_asic_wave?(pkt)
+    ([:lo_beta, :hi_beta, :delta, :lo_gamma, :theta, :mid_gamma, :lo_alpha, 
+      :hi_alpha] & pkt.keys).count > 0
+  end
+
+  def is_esense?(pkt)
+    (pkt.keys.include? :attention) || (pkt.keys.include? :meditation)
+  end
+
+  def skip_packet?(pkt, options)
+    ((pkt.keys.include? :wave) && ! options.raw) ||
+    ((pkt.keys.include? :signal_quality) && ! options.quality) ||
+    ((is_asic_wave? pkt) && ! options.wave) ||
+    ((is_esense? pkt) && ! options.esense)
+  end
+
+  def store_packets( h, packets, options )
+    packets.each { |pkt| pkt.each { |k,v| h[k] << v } }
+  end
+
+  def print_packets( packets, options )
+    label = options.multi || options.verbose
+    packets.each do |pkt|
+      pkt.each { |k,v| puts "%s%d" % [(label ? k.to_s.upcase + ': ' : ''), v] }
+    end
+  end
 end
 
-def new_packet_store
-  { 
-    :start_ts => Time.now,
-    :end_ts => nil,
-    :delta => [],
-    :theta => [],
-    :lo_alpha => [],
-    :hi_alpha => [],
-    :lo_beta => [],
-    :hi_beta => [],
-    :lo_gamma => [],
-    :mid_gamma => [],
-    :signal_quality => [],
-    :attention => [],
-    :meditation => [],
-    :blink => [],
-    :wave => []
-  }
-end
+# ----------------------------------------------------------------------
+  def read_n_bytes(conn, n)
+    bytes = []
+    n.times { bytes << conn.readbyte }
+    bytes
+  end
 
-def is_asic_wave?(pkt)
-  ([:lo_beta, :hi_beta, :delta, :lo_gamma, :theta, :mid_gamma, :lo_alpha, 
-    :hi_alpha] & pkt.keys).count > 0
-end
+  def while_not_byte(conn, val, &block)
+    cont = true
+    while cont
+      c = conn.readbyte
+      break if c == val
+      cont = yield c
+    end
+  end
 
-def is_esense?(pkt)
-  (pkt.keys.include? :attention) || (pkt.keys.include? :meditation)
-end
+  def wait_for_not_byte(conn, val, max_counter=6000)
+    counter = 0
+    c = nil
+    while counter < max_counter
+      c = conn.readbyte
+      break if (c != val)
+      counter += 1
+      sleep 0.1
+    end
 
+    raise ConnectionTimeoutError if counter >= max_counter
+    c
+  end
+
+  def wait_for_byte(conn, val, max_counter=6000)
+    counter = 0
+    while counter < max_counter
+      c = conn.readbyte
+      break if (c == val)
+      counter += 1
+      sleep 0.1
+    end
+
+    raise ConnectionTimeoutError if counter >= max_counter
+  end
+
+  def read_packet(conn, verbose=false)
+    wait_for_byte(conn, BT_SYNC, 100)
+    wait_for_byte(conn, BT_SYNC, 100)
+
+    plen = read_n_bytes(conn, 1).first
+    if plen >= BT_SYNC
+      $stderr.puts "Invalid packet size: #{plen} bytes" if verbose
+      return []
+    end
+
+    buf = read_n_bytes(conn, plen)
+
+    buf_cs = buf.inject(0) { |sum, b| sum + b } & 0xFF
+    buf_cs = ~buf_cs & 0xFF
+    checksum = read_n_bytes(conn, 1).first
+    if buf_cs != checksum
+      $stderr.puts "Packet #{buf_cs} != checksum #{checkum}" if verbose
+      return []
+    end
+
+    parse_packet buf, verbose
+  end
+=end
+
+# ----------------------------------------------------------------------
 def skip_packet?(pkt, options)
   ((pkt.keys.include? :wave) && ! options.raw) ||
   ((pkt.keys.include? :signal_quality) && ! options.quality) ||
   ((is_asic_wave? pkt) && ! options.wave) ||
   ((is_esense? pkt) && ! options.esense)
-end
-
-def store_packets( h, packets, options )
-  packets.each { |pkt| pkt.each { |k,v| h[k] << v } }
 end
 
 def print_packets( packets, options )
@@ -138,87 +204,16 @@ def print_packets( packets, options )
   end
 end
 
-# ----------------------------------------------------------------------
-def read_n_bytes(conn, n)
-  bytes = []
-  n.times { bytes << conn.readbyte }
-  bytes
-end
+def read_mindset_bt(options)
+  conn = Mindset.connect(options.device, options.verbose)
+  return if ! conn
 
-def while_not_byte(conn, val, &block)
-  cont = true
-  while cont
-    c = conn.readbyte
-    break if c == val
-    cont = yield c
-  end
-end
-
-def wait_for_not_byte(conn, val, max_counter=6000)
-  counter = 0
-  c = nil
-  while counter < max_counter
-    c = conn.readbyte
-    break if (c != val)
-    counter += 1
-    sleep 0.1
-  end
-
-  raise ConnectionTimeoutError if counter >= max_counter
-  c
-end
-
-def wait_for_byte(conn, val, max_counter=6000)
-  counter = 0
-  while counter < max_counter
-    c = conn.readbyte
-    break if (c == val)
-    counter += 1
-    sleep 0.1
-  end
-
-  raise ConnectionTimeoutError if counter >= max_counter
-end
-
-def read_packet(conn, verbose=false)
-  wait_for_byte(conn, BT_SYNC, 100)
-  wait_for_byte(conn, BT_SYNC, 100)
-
-  plen = read_n_bytes(conn, 1).first
-  if plen >= BT_SYNC
-    $stderr.puts "Invalid packet size: #{plen} bytes" if verbose
-    return []
-  end
-
-  buf = read_n_bytes(conn, plen)
-
-  buf_cs = buf.inject(0) { |sum, b| sum + b } & 0xFF
-  buf_cs = ~buf_cs & 0xFF
-  checksum = read_n_bytes(conn, 1).first
-  if buf_cs != checksum
-    $stderr.puts "Packet #{buf_cs} != checksum #{checkum}" if verbose
-    return []
-  end
-
-  parse_packet buf, verbose
-end
-
-def read_bt_data(options)
-  $stderr.puts "CONNECT #{options.device}, #{MINDSET_BAUD}" if options.verbose
-  bt = nil
-  begin
-    bt = SerialPort.new options.device, MINDSET_BAUD
-  rescue TypeError => e
-    $stderr.puts "Could not connect to #{options.device}: #{e.message}"
-    return
-  end
-
-  h_pkt = new_packet_store
+  h_pkt = Mindset::PacketStore.new
   num = 0
   cont = true
   while cont
     begin
-      packets = read_packet(bt, options.verbose).reject { |pkt| 
+      packets = conn.read_packet(options.verbose).reject { |pkt| 
                 skip_packet? pkt, options }
       num += packets.length
       if options.json
@@ -231,10 +226,12 @@ def read_bt_data(options)
       cont = false if (options.seconds && 
                        Time.now - h_pkt[:start_ts] >= options.seconds)
 
-    rescue ConnectionTimeoutError, Interrupt => e
-      cont = false
+      rescue Mindset::Connection::TimeoutError, Interrupt => e
+        cont = false
     end
   end
+
+  conn.disconnect
 
   if options.json
     h_pkt[:end_ts] = Time.now
@@ -242,7 +239,6 @@ def read_bt_data(options)
   end
 end
 
-# ----------------------------------------------------------------------
 def get_options(args)
   options = OpenStruct.new
   options.esense = false
@@ -297,5 +293,5 @@ def get_options(args)
 end
 
 if __FILE__ == $0
-  read_bt_data(get_options ARGV)
+  read_mindset_bt(get_options ARGV)
 end
